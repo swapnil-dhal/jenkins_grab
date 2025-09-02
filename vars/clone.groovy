492f830a1664e19d550e1d4296a405ce2fd2b99e
swapnil-dhal <swapnildhal2004@gmail.com>
2025-09-02 23:43:12 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,3 +1,8 @@
 def call() {
-    git branch: 'main',  url: 'https://github.com/swapnil-dhal/django-notes-app.git'
+    sh """
+        echo "Starting clone process..."
+        rm -rf django-notes-app
+        git clone https://github.com/swapnil-dhal/django-notes-app.git
+        ls -la django-notes-app/
+    """
 }
