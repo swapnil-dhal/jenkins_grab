0681b8de55a0d4f503ddda1490bda2f14afe1302
swapnil-dhal <swapnildhal2004@gmail.com>
2025-09-02 21:38:53 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,7 +1,5 @@
 def call(){
-  sh 'cd ~/Desktop'
+
   echo "Cloning from github.."
   sh 'git clone https://github.com/swapnil-dhal/django-notes-app.git'
-  sh 'cd ~/Desktop/django-notes-app'
-  sh 'pwd'
 }
