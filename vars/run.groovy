58d683180463eb1dd9ce87a99b072e6e441a95a5
swapnil-dhal <swapnildhal2004@gmail.com>
2025-09-02 22:39:13 +0530

    Update run.groovy

--- vars/run.groovy
+++ vars/run.groovy
@@ -1,10 +1,9 @@
 def call(){
-       dir('/home/swapnil/Desktop/django-notes-app') {
+
            sh '''
                     pwd
                     ls -la
                     docker run -d -p 8000:8000 notes-app:latest
                 '''
-}
   echo "run completed"
 }
