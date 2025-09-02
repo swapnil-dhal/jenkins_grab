a753712a6390860bf6071d22e77d4612b80c0959
swapnil-dhal <swapnildhal2004@gmail.com>
2025-09-02 23:33:16 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,12 +1,5 @@
-def call(String repoUrl = 'https://github.com/swapnil-dhal/django-notes-app.git', String branch = 'main') {
+def call() {
     sh """
-        echo "=== Cloning Django Application from GitHub ==="
-        git clone -b ${branch} ${repoUrl} .
-        
-        echo "=== Repository cloned successfully ==="
-        ls -la
-        
-        echo "=== Git information ==="
-        git log --oneline -5
+       git clone https://github.com/swapnil-dhal/django-notes-app.git
     """
 }
