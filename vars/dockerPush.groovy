60baf4df4cfa7a1dae029b956f7d362320de73b4
swapnil-dhal <swapnildhal2004@gmail.com>
2025-09-04 19:58:07 +0530

    Create dockerPush.groovy

---
+++ vars/dockerPush.groovy
@@ -0,0 +1,10 @@
+def dockerPush(credentialsId, appname, appversion) {
+    withCredentials([usernamePassword(credentialsId: credentialsId, 
+                                     usernameVariable: 'DOCKER_USER', 
+                                     passwordVariable: 'DOCKER_PASS')]) {
+        sh """
+            echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin
+            docker push ${appname}:${appversion}
+        """
+    }
+}
